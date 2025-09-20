8cb3e50c09ef089f3cb6ca41559ae0e85cb6b173
Abdelrahman.Mohamed <Abdelrahman.Mohamed@DESKTOP-4JQT4TT>
2025-09-20 13:51:04 +0300

    Add project files.

---
+++ Globo.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using Globo.TicketManagement.Domain.Comman;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globo.TicketManagement.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
---
+++ Globo.TicketManagement.Domain/Entities/Event.cs
@@ -0,0 +1,18 @@
+﻿using Globo.TicketManagement.Domain.Comman;
+
+namespace Globo.TicketManagement.Domain.Entities
+{
+    public class Event : AuditableEntity
+    {
+        public Guid EId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Artist { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
